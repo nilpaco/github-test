a7cb3afc0fe6d538cbadbdfeb2b1a2ab42c2768e
Nil Panés <nil.panes@gmail.com>
2018-02-04 19:21:37 +0100

    fix class user and readme

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,6 +2,8 @@ export class User {
     public id: number;
     public login: string;
     public avatar_url: string;
+    public name: string;
+    public bio?: string;
     public followers_url?: string;
     public repos_url?: string;
     public followersList?: Array<any>;
